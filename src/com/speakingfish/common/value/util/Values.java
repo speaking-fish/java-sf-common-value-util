fe8411afe1dee92b3e62cccdeda46a838fd4e5dc
speakingfish <speakingfish@gmail.com>
2016-02-24 15:32:41 +0300

    input parameter Creator->Mapper + simpleSingleton shorthand

--- src/com/speakingfish/common/value/util/Values.java
+++ src/com/speakingfish/common/value/util/Values.java
@@ -1,7 +1,7 @@
 package com.speakingfish.common.value.util;
 
-import com.speakingfish.common.function.Creator;
 import com.speakingfish.common.function.Getter;
+import com.speakingfish.common.function.Mapper;
 import com.speakingfish.common.function.ThrowableGetter;
 
 public class Values {
@@ -24,51 +24,59 @@ public class Values {
         };
     }
     
-    public static <RESULT, PARAMS> Getter<RESULT> threadLocalCloseableSingleton(final Creator<RESULT, PARAMS> creator, final PARAMS params) {
+    public static <RESULT, PARAMS> Getter<RESULT> simpleSingleton(final Mapper<RESULT, PARAMS> creator, final PARAMS params) {
+        return new AbstractSimpleSingleton<RESULT>() {
+            @Override protected RESULT create() {
+                return creator.apply(params);
+            }
+        };
+    }
+
+    public static <RESULT, PARAMS> Getter<RESULT> threadLocalCloseableSingleton(final Mapper<RESULT, PARAMS> creator, final PARAMS params) {
         return new AbstractThreadLocalCloseableSingleton<RESULT>() {
             @Override protected RESULT create() {
-                return creator.create(params);
+                return creator.apply(params);
             }
         };
     }
     
-    public static <RESULT, SRC> Getter<RESULT> threadLocalCloseableSingleton(final Creator<RESULT, SRC> creator, final Getter<SRC> getter) {
+    public static <RESULT, SRC> Getter<RESULT> threadLocalCloseableSingleton(final Mapper<RESULT, SRC> creator, final Getter<SRC> getter) {
         return new AbstractThreadLocalCloseableSingleton<RESULT>() {
             @Override protected RESULT create() {
-                return creator.create(getter.get());
+                return creator.apply(getter.get());
             }
         };
     }
     
-    public static <RESULT, PARAMS> SimpleCloseableSingleton<RESULT> simpleCloseableSingleton(final Creator<RESULT, PARAMS> creator, final PARAMS params) {
+    public static <RESULT, PARAMS> SimpleCloseableSingleton<RESULT> simpleCloseableSingleton(final Mapper<RESULT, PARAMS> creator, final PARAMS params) {
         return new AbstractSimpleCloseableSingleton<RESULT>() {
             @Override protected RESULT create() {
-                return creator.create(params);
+                return creator.apply(params);
             }
         };
     }
     
-    public static <RESULT, PARAMS> SimpleCloseableSingleton<RESULT> lazySimpleCloseableSingleton(final Creator<RESULT, PARAMS> creator, final Getter<PARAMS> paramsGetter) {
+    public static <RESULT, PARAMS> SimpleCloseableSingleton<RESULT> lazySimpleCloseableSingleton(final Mapper<RESULT, PARAMS> creator, final Getter<PARAMS> paramsGetter) {
         return new AbstractSimpleCloseableSingleton<RESULT>() {
             @Override protected RESULT create() {
-                return creator.create(paramsGetter.get());
+                return creator.apply(paramsGetter.get());
             }
         };
     }
     
-    public static <RESULT, SRC> Getter<RESULT> wrappedSimpleCloseableSingleton(final Creator<RESULT, SRC> creator, final Getter<SRC> getter) {
+    public static <RESULT, SRC> Getter<RESULT> wrappedSimpleCloseableSingleton(final Mapper<RESULT, SRC> creator, final Getter<SRC> getter) {
         return new AbstractSimpleCloseableSingleton<RESULT>() {
             @Override protected RESULT create() {
-                return creator.create(getter.get());
+                return creator.apply(getter.get());
             }
         };
     }
     
-    public static <RESULT> Getter<RESULT> threadLocalCloseableSingleton(final Creator<RESULT, Void> creator) {
+    public static <RESULT> Getter<RESULT> threadLocalCloseableSingleton(final Mapper<RESULT, Void> creator) {
         return Values.threadLocalCloseableSingleton(creator, (Void) null); // Void for java 1.5 compatibility
     }
     
-    public static <RESULT> SimpleCloseableSingleton<RESULT> simpleCloseableSingleton(final Creator<RESULT, Void> creator) {
+    public static <RESULT> SimpleCloseableSingleton<RESULT> simpleCloseableSingleton(final Mapper<RESULT, Void> creator) {
         return simpleCloseableSingleton(creator, null);
     }
 
